52eaacb56ce09007ee7744b88de2bb1753a13c7e
Fujimatos <dupont.thibaut.ilejeanty@gmail.com>
2023-01-25 11:35:04 +0100

    test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-var basic = document.querySelector("button--basic")
+const eclate = document.querySelector(".tangram")
+const boutton_eclate = document.querySelector(".boutton--eclate")
+const boutton_carre = document.querySelector(".boutton--carre")
+const boutton_perso = document.querySelector(".boutton--perso")
 
-document.addEventListener("click", function(){
-    alert("coucou")
-  });
+
+
+  boutton_eclate.addEventListener("click",
+  function(){ 
+    eclate.classList.add("tangram--eclate")
+    eclate.classList.remove("tangram--square")
+    eclate.classList.remove("tangram--perso")
+ })
+  boutton_carre.addEventListener("click",
+  function(){ 
+    eclate.classList.add("tangram--square")
+    eclate.classList.remove("tangram--eclate")
+    eclate.classList.remove("tangram--perso")
+
+ })
+  boutton_perso.addEventListener("click",
+  function(){ 
+    eclate.classList.add("tangram--perso")
+    eclate.classList.remove("tangram--square")
+    eclate.classList.remove("tangram--eclate")
+  })
+ 
+
+
